b257f5ddbbc211b1b8320723aed67f8e210069d6
Ayan Dhara <37933140+Ayan-Dhara@users.noreply.github.com>
2021-07-24 00:29:05 +0530

    Added most of the movements

--- client/src/pages/room/context/BoardCells.js
+++ client/src/pages/room/context/BoardCells.js
@@ -1,12 +1,9 @@
 "use strict"
-import {setBoardReady} from "./RoomContext.js";
-
 let listenerCallback = ()=>{}
 export let currentBoardCells = [...Array(8)].map(()=>[...Array(8)])
 
 export function register(func){
   listenerCallback = func
-  setBoardReady(true)
 }
 
 export function change(boardCells){
--- client/src/pages/room/context/RoomContext.js
+++ client/src/pages/room/context/RoomContext.js
@@ -1,6 +1,6 @@
 "use strict"
 
-import {change as changeBoardPieces, currentBoardPieces} from './BoardPieces.js'
+import {change as changeBoardPieces, currentBoardPieces} from './BoardPieces'
 import {change as changeBoardCells, currentBoardCells} from "./BoardCells";
 
 import {KEYWORDS} from "../../../KeyWords";
@@ -9,9 +9,15 @@ const LOCAL_PORT = 5000
 const socket = new WebSocket(`${window.location.protocol.replace("http", "ws")}//${window.location.hostname}:${(window.location.hostname==="localhost"||window.location.hostname.toString().startsWith("127.0.0"))?LOCAL_PORT:window.location.port}/ws${window.location.pathname}`)
 
 
+// initiate communication
 let boardReady = false;
 export function setBoardReady(status){boardReady = status}
 const requestInitialBoard = () => {
+  const userName = localStorage.getItem("username")
+  if (!userName){
+    // show name input popup
+    // return
+  }
   if (!boardReady){
     setTimeout(()=>{
       requestInitialBoard()
@@ -28,6 +34,14 @@ socket.addEventListener("open", ()=>{
 })
 
 
+// handle incoming message
+socket.addEventListener("message", (event) =>{
+  let message = {}
+  try {
+    message = JSON.parse(event.data)
+  }catch(e){}
+  handleMessage(message)
+})
 const handleMessage = (message)=>{
   switch (message.type){
     case KEYWORDS.MOVE: {
@@ -49,10 +63,66 @@ const handleMessage = (message)=>{
 }
 
 
+// handle piece click
+// 0 once
+// 1 infinite
+// 2 move and cut different
+// 3 special
+export const pieceMoves = {
+  K: {
+    move: [
+      [-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]
+    ],
+    castling: [
+      [0, 1], [0,-1]
+    ],
+    type: 3
+  },
+  Q: {
+    move: [
+      [-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]
+    ],
+    type: 1
+  },
+  R: {
+    move: [
+      [-1, 0], [0, -1], [0, 1], [1, 0],
+    ],
+    type: 1
+  },
+  B: {
+    move: [
+      [-1, -1], [-1, 1], [1, -1], [1, 1]
+    ],
+    type: 1
+  },
+  N:  {
+    move: [
+      [-1, 2], [-1, -2], [1, 2], [1, -2], [-2, 1], [-2, -1], [2, 1], [2, -1]
+    ],
+    type: 0
+  },
+  P:  {
+    initial: [
+      [-1, 0], [-2, 0]
+    ],
+    move: [
+      [-1, 0]
+    ],
+    cut: [
+      [-1, 1], [-1, -1]
+    ],
+    type: 3
+  }
+}
+let castlingUsed = false
+let leftRookMoved = false
+let rightRookMoved = false
+let kingMoved = false
 export const onClickPiece = (event) => {
   const piece = event.target
-  const i = piece.dataset.i*1
-  const j = piece.dataset.j*1
+  const i = piece.dataset.i * 1
+  const j = piece.dataset.j * 1
   const type = piece.dataset.piece
   removeHighlights()
   if(movingPiece === piece){
@@ -60,18 +130,37 @@ export const onClickPiece = (event) => {
     return
   }
   movingPiece = piece
-
   const boardCells = [...currentBoardCells]
   boardCells[i][j] = "selected"
   const moves = pieceMoves[type[1]].move
   switch (pieceMoves[type[1]].type){
+    case 0:{
+      for (let [dx, dy] of moves){
+        let x = i + dx
+        let y = j + dy
+        if(x > -1 && y > -1 && x < 8 && y < 8){
+          if(haveSameSidePiece(type, x, y)){
+            continue
+          }
+          if(haveOtherSidePiece(type, x, y)){
+            boardCells[x][y] = "red"
+          }
+          else{
+            boardCells[x][y] = "green"
+            x = x + dx
+            y = y + dy
+          }
+        }
+      }
+      break
+    }
     case 1: {
       for (let [dx, dy] of moves){
         let x = i + dx
         let y = j + dy
         while(x > -1 && y > -1 && x < 8 && y < 8){
           if(haveSameSidePiece(type, x, y)){
-            // break
+            break
           }
           if(haveOtherSidePiece(type, x, y)){
             boardCells[x][y] = "red"
@@ -80,6 +169,97 @@ export const onClickPiece = (event) => {
           boardCells[x][y] = "green"
           x = x + dx
           y = y + dy
+        }
+      }
+      break
+    }
+    default: {
+      switch(type[1]){
+        case "K": {
+          // primary move
+          for (let [dx, dy] of moves){
+            let x = i + dx
+            let y = j + dy
+            if(x > -1 && y > -1 && x < 8 && y < 8){
+              if(haveSameSidePiece(type, x, y)){
+                break
+              }
+              if(haveOtherSidePiece(type, x, y)){
+                boardCells[x][y] = "red"
+              }
+              else{
+                boardCells[x][y] = "green"
+                x = x + dx
+                y = y + dy
+              }
+            }
+          }
+          // castling
+          if(!castlingUsed && !kingMoved){
+            for (let [dx, dy] of moves){
+              let x = i + dx
+              let y = j + dy
+              if(x > -1 && y > -1 && x < 8 && y < 8){
+                if(haveSameSidePiece(type, x, y)){
+                  break
+                }
+                if(haveOtherSidePiece(type, x, y)){
+                  boardCells[x][y] = "red"
+                }
+                else{
+                  boardCells[x][y] = "green"
+                  x = x + dx
+                  y = y + dy
+                }
+              }
+            }
+          }
+          break
+        }
+        case "P": {
+          if(i === 6){
+            // initial move
+            for (let [dx, dy] of pieceMoves["P"].initial){
+              let x = i + dx
+              let y = j + dy
+              if(x > -1 && y > -1 && x < 8 && y < 8){
+                if(haveSameSidePiece(type, x, y)){
+                  break
+                }
+                if(haveOtherSidePiece(type, x, y)){
+                  break
+                }
+                boardCells[x][y] = "green"
+              }
+            }
+          }
+          else {
+            // primary move
+            for (let [dx, dy] of pieceMoves["P"].move){
+              let x = i + dx
+              let y = j + dy
+              if(x > -1 && y > -1 && x < 8 && y < 8){
+                if(haveSameSidePiece(type, x, y)){
+                  break
+                }
+                if(haveOtherSidePiece(type, x, y)){
+                  break
+                }
+                boardCells[x][y] = "green"
+              }
+            }
+          }
+          // cut move
+          for (let [dx, dy] of pieceMoves["P"].cut) {
+            let x = i + dx
+            let y = j + dy
+            if (x > -1 && y > -1 && x < 8 && y < 8) {
+              if (haveOtherSidePiece(type, x, y)) {
+                boardCells[x][y] = "red"
+              }
+            }
+          }
+          break
         }
       }
     }
@@ -110,6 +290,7 @@ const haveOtherSidePiece = (type, x, y)=>{
 }
 
 
+// handle cell click
 export let movingPiece = null
 export const onClickCell = (x, y) =>{
   if(!movingPiece)
@@ -141,100 +322,3 @@ export const removeHighlights = () => {
   }
   changeBoardCells(boardCells)
 }
-
-// 0 once
-// 1 infinite
-// 2 move and cut different
-// 3 special
-export const pieceMoves = {
-  K: {
-    move: [
-      [-1, -1],
-      [-1, 0],
-      [-1, 1],
-      [0, -1],
-      [0, 1],
-      [1, -1],
-      [1, 0],
-      [1, 1]
-    ],
-    castling: [
-      [0, 1],
-      [0,-1]
-    ],
-    type: 3
-  },
-  Q: {
-    move: [
-      [-1, -1],
-      [-1, 0],
-      [-1, 1],
-      [0, -1],
-      [0, 1],
-      [1, -1],
-      [1, 0],
-      [1, 1]
-    ],
-    type: 1
-  },
-  R: {
-    move: [
-      [-1, 0],
-      [0, -1],
-      [0, 1],
-      [1, 0],
-    ],
-    type: 1
-  },
-  B: {
-    move: [
-      [-1, -1],
-      [-1, 1],
-      [1, -1],
-      [1, 1]
-    ],
-    type: 1
-  },
-  N:  {
-    move: [
-      [-1, 2],
-      [-1, -2],
-      [1, 2],
-      [1, -2],
-      [-2, 1],
-      [-2, -1],
-      [2, 1],
-      [2, -1]
-    ],
-    type: 0
-  },
-  P:  {
-    initial: [
-      [-1, 0],
-      [-2, 0]
-    ],
-    move: [
-      [-1, 0]
-    ],
-    cut: [
-      [-1, 1],
-      [-1, -1]
-    ],
-    type: 3
-  }
-}
-
-
-
-
-
-socket.addEventListener("message", (event) =>{
-  let message = {}
-  try {
-    message = JSON.parse(event.data)
-  }catch(e){}
-  handleMessage(message)
-})
-
-
-
